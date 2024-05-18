482a79683ec108bd5f959dc81e32c5bb7be92148
GeheimCoding <geheim.coding@gmail.com>
2024-05-18 22:23:34 +0200

    Add fly cam

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,22 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use bevy::prelude::*;
+use bevy_flycam::{FlyCam, MovementSettings, NoCameraPlayerPlugin};
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
+        .add_plugins(NoCameraPlayerPlugin)
+        .insert_resource(MovementSettings {
+            sensitivity: 0.00018,
+            speed: 42.0,
+        })
         .add_systems(Startup, setup)
         .run();
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    let lantern = asset_server.load("models/Lantern.glb#Scene0");
+    let lantern = asset_server.load("models/Building_Brutalist_TriCorner.glb#Scene0");
     commands.spawn(SceneBundle {
         scene: lantern,
         ..default()
@@ -22,13 +28,16 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
             shadows_enabled: true,
             ..default()
         },
-        transform: Transform::from_xyz(12.0, 14.0, 6.0),
+        transform: Transform::from_xyz(10.0, 20.0, 20.0),
         ..default()
     });
 
-    commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(40.0, 20.0, 20.0)
-            .looking_at(Vec3::new(0.0, 12.0, 0.0), Vec3::Y),
-        ..default()
-    });
+    commands.spawn((
+        Camera3dBundle {
+            transform: Transform::from_xyz(40.0, 70.0, 80.0)
+                .looking_at(Vec3::new(0.0, 20.0, 0.0), Vec3::Y),
+            ..default()
+        },
+        FlyCam,
+    ));
 }
